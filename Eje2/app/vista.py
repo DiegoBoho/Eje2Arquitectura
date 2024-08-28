deb85241c8e1963bb02c94f6ef167766cb42f9bc
Alejandro Bohorquez <diego.bohorquez.mt@correo.usa.edu.co>
2024-08-27 23:37:23 -0500

    agrego vista con metodos get probados, faltan probar los put, delete y post

--- Eje2/app/vista.py
+++ Eje2/app/vista.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, jsonify, request, render_template
-from .controlador import crear_usuario, obtener_usuarios, obtener_usuario_por_id, actualizar_usuario, eliminar_usuario,obtener_clientes,obtener_cliente_por_id,crear_cliente,actualizar_cliente,eliminar_cliente
+from .controlador import crear_usuario, obtener_usuarios, obtener_usuario_por_id, actualizar_usuario, eliminar_usuario
+from .controlador import obtener_clientes,obtener_cliente_por_id,crear_cliente,actualizar_cliente,eliminar_cliente
+from .controlador import listar_productos,crear_productos,obtener_productos_por_id,actualizar_productos,eliminar_productos
+from .controlador import crear_factura,listar_factura,obtener_factura_por_id,actualizar_factura,eliminar_factura
+from .controlador import crear_factura_detalles,listar_detalles_factura,actualizar_factura_detalles,eliminar_factura_detalles,obtener_detalles_factura_por_id
 
 main_blueprint = Blueprint('main', __name__)
 
@@ -71,3 +75,93 @@ def borrar_cliente(id):
         return jsonify({'mensaje': 'Cliente eliminado exitosamente'})
     return jsonify({'mensaje': 'Error, cliente no encontrado'})
 
+
+@main_blueprint.route('/productos',methods=['GET'])
+def lista_productos():
+    productos = listar_productos()
+    return jsonify([{'id_producto':u.id_producto,'Nombre_producto':u.Nombre_producto,'precio':u.Precio}for u in productos])
+
+@main_blueprint.route('/productos/<int:id>', methods=['GET'])
+def obtener_producto_id(id):
+    p = obtener_productos_por_id(id)
+    return jsonify({'id_producto':p.id_producto,'Nombre_producto':p.Nombre_producto,'Precio':p.Precio})
+
+@main_blueprint.route('/productos', methods=['POST'])
+def crear_producto():
+    data = request.json
+    crear_productos(data['id_producto'], data['Nombre_producto'], data['Precio'])
+    return jsonify({'mensaje': 'Producto creado exitosamente'}), 201 
+   
+@main_blueprint.route('/productos/<int:id>', methods=['PUT'])
+def actualizar_producto(id):
+    data = request.json
+    actualizar_productos(id, data['Nombre_producto'], data['Precio'])
+    return jsonify({'mensaje': 'Producto actualizado exitosamente'})
+
+@main_blueprint.route('/productos/<int:id>', methods=['DELETE'])
+def borrar_productos(id):
+    productos = obtener_productos_por_id(id)
+    if productos:
+        eliminar_productos(id)
+        return jsonify({'mensaje': 'Producto eliminado exitosamente'})
+    return jsonify({'mensaje': 'Error, producto no encontrado'})
+
+@main_blueprint.route('/facturas',methods=['GET'])
+def lista_facturas():
+    facturas = listar_factura()
+    return jsonify([{'id_Factura':u.id_Factura,'Fecha_Generacion':u.Fecha_Generacion,'id_Usuario':u.id_Usuario,'id_Cliente':u.id_Cliente}for u in facturas])
+
+@main_blueprint.route('/facturas/<int:id>', methods=['GET'])
+def obtener_factura_id(id):
+    p = obtener_factura_por_id(id)
+    return jsonify({'id_Factura':p.id_Factura,'Fecha_Generacion':p.Fecha_Generacion,'id_Usuario':p.id_Usuario,'id_Cliente':p.id_Cliente})
+
+@main_blueprint.route('/facturas', methods=['POST'])
+def crear_facturas():
+    data = request.json
+    crear_factura(data['id_factura'], data['Fecha_generacion'], data['id_Usuario'], data['id_Cliente'])
+    return jsonify({'mensaje': 'Factura creado exitosamente'}), 201
+
+@main_blueprint.route('/facturas/<int:id>', methods=['PUT'])
+def actualizar_factura(id):
+    data = request.json
+    actualizar_factura(id, data['Fecha_generacion'], data['id_Usuario'], data['id_Cliente'])
+    return jsonify({'mensaje': 'Factura actualizado exitosamente'})
+
+@main_blueprint.route('/facturas/<int:id>', methods=['DELETE'])
+def borrar_factura(id):
+    facturas = obtener_factura_por_id(id)
+    if facturas:
+        eliminar_factura(id)
+        return jsonify({'mensaje': 'Factura eliminado exitosamente'})
+    return jsonify({'mensaje': 'Error, Factura no encontrado'})
+
+@main_blueprint.route('/facturasdetalles/<int:id>', methods=['GET'])
+def obtener_factura_detalles(id):
+    p = obtener_detalles_factura_por_id(id)
+    return jsonify({'id_detalle_factura':p.id_Detalle_Factura,'id_producto':p.id_producto,'cantidad':p.cantidad,'precio_total':p.precio_total,'id_Factura':p.id_Factura})
+
+@main_blueprint.route('/facturasdetalles', methods=['GET'])
+def lista_factura_detalles():
+    lf = listar_detalles_factura()
+    return jsonify([{'id_Detalle_Factura':u.id_Detalle_Factura,'id_producto':u.id_producto,'cantidad':u.cantidad,'precio_total':u.precio_total,'id_Factura':u.id_Factura}for u in lf])
+
+@main_blueprint.route('/facturasdetalles', methods=['POST'])
+def crear_factura_detalle():
+    data = request.json
+    crear_factura_detalles(data['id_Detalle_Factura'], data['id_producto'], data['cantidad'], data['precio_total'], data['id_Factura'])
+    return jsonify({'mensaje': 'Factura Detalle creado exitosamente'}), 201
+
+@main_blueprint.route('/facturasdetalles/<int:id>', methods=['PUT'])
+def editar_factura_detalle(id):
+    data = request.json
+    actualizar_factura_detalles(id, data['id_Factura'], data['id_producto'], data['cantidad'], data['precio_total'])
+    return jsonify({'mensaje': 'Factura Detalle actualizado exitosamente'})
+
+@main_blueprint.route('/facturasdetalles/<int:id>', methods=['DELETE'])
+def borrar_factura_detalles(id):
+    facturas = obtener_productos_por_id(id)
+    if facturas:
+        eliminar_factura_detalles(id)
+        return jsonify({'mensaje': 'Factura Detalle eliminado exitosamente'})
+    return jsonify({'mensaje': 'Error, Factura Detalle no encontrado'})
